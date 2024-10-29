c08a11b98f9430616c9d05eb22dd994a4eb0f25d
Sergespb957 <sgudimov31@gmail.com>
2024-10-29 19:36:19 +0300

    1

---
+++ exam-3.py
@@ -0,0 +1,42 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, node, key):
+        if node is None:
+            return TreeNode(key)
+        if key < node.key:
+            node.left = self._insert_recursive(node.left, key)
+        else:
+            node.right = self._insert_recursive(node.right, key)
+        return node
+
+    def search(self, key):
+        return self._search_recursive(self.root, key)
+
+    def _search_recursive(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search_recursive(node.left, key)
+        return self._search_recursive(node.right, key)
+
+    def inorder_traversal(self):
+        return self._inorder_recursive(self.root)
+
+    def _inorder_recursive(self, node):
+        result = []
+        if node:
+            result += self._inorder_recursive(node.left)
+            result.append(node.key)
+            result += self._inorder_recursive(node.right)
+        return result
